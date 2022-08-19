4b0a175e1962b9bb7949b279ece6af48ee4caa80
jiqingwu <jiqingwu@weihaizixun.com>
2022-08-19 16:11:52 +0800

    pinia

---
+++ src/components/usePinia.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    {{ userStore.name }}
+  </div>
+</template>
+
+<script setup>
+import { useUserStore } from '@/store/user'
+
+const userStore =  useUserStore()
+// 修改姓名
+userStore.updateName('李四')
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
