fbc97cf377358aa5ce79b9b2fcd42cbb3a32c407
jiqingwu <jiqingwu@weihaizixun.com>
2022-08-19 15:32:03 +0800

    eslint ts

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  parser: 'vue-eslint-parser',
+
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 2020,
+    sourceType: 'moudle',
+    ecmaFeature: {
+      jsx: true
+    }
+  },
+
+  extends: [
+    'plugin:vue/vue3-recommended',
+    'plugin"@typescript-eslint/recommended',
+  ],
+  ruels: {
+
+  }
+}
